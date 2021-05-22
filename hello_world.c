6bebfa7bfe582e6b95279aae90c05f7380582c48
capps096github <capps096github@gmail.com>
2021-05-22 22:59:38 +0800

    ythe cephas C

--- hello_world.c
+++ hello_world.c
@@ -4,6 +4,6 @@ int main(){
 
     printf("Hello Cephas! Github is Here to Try");
 
-printf("If others can then why can't  Cephas");
+printf("If others can then why can't  Cephas Brian");
     return 0;
 }
